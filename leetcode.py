9c7f3854a430430727a3df36643ccf8d794b305d
Salman <85234357+Salman19861@users.noreply.github.com>
2022-11-29 12:13:36 +0500

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -33,6 +33,34 @@ print(twoSum([2,51,8,11],10))
 # Output: [0]
 
 # Definition for singly-linked list.
+
+
+# Question No: 13
+def romanToInt( s: str) -> int:
+
+    result=0
+    myd={
+        'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,
+    }
+    s=s.replace('IV','IIII').replace('IX','VIIII').replace('XL','XXXX').replace('XC','XXXXXXXXX').replace('CD','CCCC').replace('CM','DCCC')
+    print(s)
+    return sum(map(lambda x:myd[x],s))
+print(romanToInt('MCMXCIV'))
+# 
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_to_integer = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        s = s.replace("IV", "IIII").replace("IX", "VIIII").replace("XL", "XXXX").replace("XC", "LXXXX").replace("CD", "CCCC").replace("CM", "DCCCC")
+        return sum(map(lambda x: roman_to_integer[x], s))
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
